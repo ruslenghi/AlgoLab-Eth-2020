025865f75d5fe94311bcb9bb45449da5171ca7b3
Riccardo Uslenghi <55544485+ruslenghi@users.noreply.github.com>
2020-12-28 11:00:38 +0100

    Update NewYork.cpp

--- Week10/NewYork.cpp
+++ Week10/NewYork.cpp
@@ -1,72 +1,90 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<set>
+#include<queue>
 
 void testcase(){
   int n, m, k; std::cin >> n >> m >> k;
-  std::vector<int> T;
-  for(int i=0; i<n; i++){
-    int h; std::cin >> h;
-    T.push_back(h);
-  }
   
-  std::vector<int> routes(n,-1);
-  for(int i=0; i<n-1; i++){
-    int u, v; std::cin >> u >> v;
-    routes[v] = u; 
-  }
-  
-  std::vector<int> feasible_points;
-  for(int i=n-1; i>0; i--){
-    int steps = 0;
-    int my_place = i;
-    int max_temp = T[i];
-    int min_temp = T[i];
-    while(steps<m-1 and my_place != 0){
-      my_place = routes[my_place];
-      max_temp = std::max(T[my_place], max_temp);
-      min_temp = std::min(T[my_place], min_temp);
-      if(max_temp-min_temp > k) break;
-      else steps++;
-    }
-    
-    if(steps == m-1) feasible_points.push_back(my_place);
-    
-  }
-  
-  int f_size = feasible_points.size();
-  if(f_size == 0) {
+  if(n<m) {
     std::cout << "Abort mission" << "\n";
     return;}
-    
-  if(f_size == 1) {
-    std::cout << feasible_points[0] << "\n";
-    return;}
-    
-  std::sort(feasible_points.begin(),feasible_points.end());
-  std::vector<int> my_final_points;
   
-  for(int i=0; i<f_size-1; i++){
-    int a = feasible_points[i];
-    int b = feasible_points[i+1];
-    if(a != b) my_final_points.push_back(a);
+  std::vector<int> T;
+  std::vector<int> routes(n,-1);
+  std::vector<bool> is_parent(n,false);
+  std::vector<bool> visited(n,false);
+  
+  for(int i=0; i<n; i++){
+    int my_t; std::cin >> my_t;
+    T.push_back(my_t);
   }
   
-  int my_size = my_final_points.size();
+  for(int i=0; i<n-1; i++){
+    int parent, child; std::cin >> parent >> child;
+    routes[child] = parent;
+    is_parent[parent] = true;
+  }
   
-  if(feasible_points[f_size-1] != my_final_points[my_size-1]){
-    my_final_points.push_back(feasible_points[f_size-1]);
-    my_size += 1;}
+  std::queue<int> Q;
+  for(int i=0; i<n; i++){
+    if(!is_parent[i]) Q.push(i);
+  }
 
-  for(int i=0; i < my_size; i++){
-    std::cout << my_final_points[i] << " ";
+  std::vector<int> feasible_points;
+  while(!Q.empty()){
+    
+    std::multiset<long> window;
+    int tail = Q.front(); Q.pop();
+    
+    window.insert(T[tail]);
+    
+    int head = tail;
+    //Be careful, in case n < m;
+    //As I already inserted one Temperature I add m-1 terms
+    for(int i=0; i<m-1; i++){
+      head = routes[head];
+      window.insert(T[head]);
+    }
+    
+    while(head != -1 and !visited[tail]){
+      
+      int mini = *window.begin();
+      int maxi = *window.rbegin();
+      if(maxi-mini<=k){feasible_points.push_back(head);}
+      
+      //The fact that I do it here should ensure that the !visited[tail] 
+      //condition is not automatically satisfied 
+      visited[tail] = true;
+      window.erase(window.find(T[tail]));
+      
+      head = routes[head];
+      window.insert(T[head]);
+      tail = routes[tail];
+      
+    }
+    
   }
+  
+  if(feasible_points.empty()){
+    std::cout << "Abort mission" << "\n";
+    return;}
+  
+  std::sort(feasible_points.begin(),feasible_points.end());
+  std::cout << feasible_points[0] << " ";
+  for(int i=1; i<feasible_points.size(); i++){
+      if(feasible_points[i] != feasible_points[i-1])
+      std::cout << feasible_points[i] << " ";
+  }
+  
   
   std::cout << "\n";
+  
 }
 
 int main(){
-  std::ios_base::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
   int t; std::cin >> t;
   for(int i=0; i<t; i++){
     testcase();
