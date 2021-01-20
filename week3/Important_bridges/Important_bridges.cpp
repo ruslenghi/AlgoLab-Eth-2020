bda2824311743e55d9d3d408f77d60b4f04a0a04
Riccardo Uslenghi <55544485+ruslenghi@users.noreply.github.com>
2021-01-20 21:06:00 +0100

    Update Important_bridges.cpp

--- week3/Important_bridges/Important_bridges.cpp
+++ week3/Important_bridges/Important_bridges.cpp
@@ -2,54 +2,66 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/biconnected_components.hpp>
 
-using namespace std;
+namespace boost
+{
+struct edge_component_t
+{
+    enum
+    {
+        num = 555
+    };
+    typedef edge_property_tag kind;
+} edge_component;
+}
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,  
-    boost::no_property,  
-    boost::property<boost::edge_weight_t, int>> Graph;
+using namespace boost;
+
+typedef adjacency_list< vecS, vecS, undirectedS, no_property,
+        property<edge_component_t, std::size_t >> Graph;
 typedef boost::graph_traits<Graph>::edge_iterator EdgeIt;
-typedef boost::property_map<Graph, boost::edge_weight_t>::type  WeightMap;
-  
-typedef pair<int,int> E;
 
 void testcase(){
   int n, m;
-  cin >> n >> m;
+  std::cin >> n >> m;
   Graph G(n);
   
-  //I define this variable, because I need a property map to store the 
-  //bicconnected components
-  WeightMap componentmap = get(boost::edge_weight, G);
+  //I create the graph
   for(int i=0; i<m; i++){
-    int e_1, e_2; std::cin >> e_1 >> e_2;
-    add_edge(e_1,e_2,G);
+    int a, b; std::cin >> a >> b;
+    boost::add_edge(a, b, G);
   }
   
-  int numcomp = boost::biconnected_components(G, componentmap);
-  std::vector<int> component_counter(numcomp,0);
-  std::vector<E> critical_edges;
-  EdgeIt ebeg, eend;
-  for(boost::tie(ebeg,eend) = edges(G); ebeg!= eend; ++ebeg){
-    int index = componentmap[*ebeg];
-    component_counter[index] += 1;
+  property_map<Graph, edge_component_t >::type component = get(edge_component, G);
+  int num_comps = biconnected_components(G, component);
+  
+  //I am going to use this for loop to assign each component with its number of edges
+  std::vector<int> num_per_comp(num_comps, 0);
+  
+  graph_traits<Graph>::edge_iterator ei, ei_end;
+  for (boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei){
+    num_per_comp[component[*ei]] += 1;
+  } 
+  
+  int counter = 0;
+  std::vector<std::pair<int,int>> important_bridges;
+  for (boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei){
+    if(num_per_comp[component[*ei]] == 1){
+      counter += 1;
+      int a = source(*ei, G);
+      int b = target(*ei, G);
+      if(b < a) important_bridges.push_back(std::make_pair(b,a));
+      else important_bridges.push_back(std::make_pair(a,b));
+    } 
   }
   
-  for(boost::tie(ebeg,eend) = edges(G); ebeg!= eend; ++ebeg){
-    int index = componentmap[*ebeg];
-    if(component_counter[index] == 1){
-      int a = boost::source(*ebeg,G);
-      int b = boost::target(*ebeg,G);
-      E couple {min(a,b),max(a,b)};
-      critical_edges.push_back(couple);
-    }
+  std::sort(important_bridges.begin(), important_bridges.end());
+  
+  std::cout << counter << "\n";
+  
+  for(int i=0; i<(int)important_bridges.size(); i++){
+    std::cout << important_bridges[i].first << " " << important_bridges[i].second << "\n";
   }
   
-  std::sort(critical_edges.begin(), critical_edges.end());
-  std::cout << critical_edges.size() << "\n";
-  for(int i =0; i < critical_edges.size(); i++){
-    std::cout << critical_edges[i].first << " " <<
-                 critical_edges[i].second << "\n";
-  }
 }
 
 int main(){
