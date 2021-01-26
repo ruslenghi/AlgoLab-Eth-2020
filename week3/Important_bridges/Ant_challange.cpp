18555d70f04065edd0676f58e42e108b47b85978
Riccardo Uslenghi <55544485+ruslenghi@users.noreply.github.com>
2021-01-26 16:01:55 +0100

    Update Ant_challange.cpp

--- week3/Important_bridges/Ant_challange.cpp
+++ week3/Important_bridges/Ant_challange.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include<climits>
+#include<boost/graph/prim_minimum_spanning_tree.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
@@ -10,12 +11,6 @@ typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
 typedef boost::property_map<weighted_graph, boost::edge_weight_t>::type weight_map;
 typedef boost::graph_traits<weighted_graph>::edge_descriptor            edge_desc;
 typedef boost::graph_traits<weighted_graph>::vertex_descriptor          vertex_desc;
-
-std::vector<edge_desc> kruskal(const weighted_graph &G) {
-  std::vector<edge_desc> mst;    
-  boost::kruskal_minimum_spanning_tree(G, std::back_inserter(mst));
-  return mst;
-}
 
 int dijkstra_dist(const weighted_graph &G, int s, int t) {
   int n = boost::num_vertices(G);
@@ -28,43 +23,46 @@ int dijkstra_dist(const weighted_graph &G, int s, int t) {
   return dist_map[t];
 }
 
-
 void testcase(){
   int n, e, s, a, b; std::cin >> n >> e >> s >> a >> b;
   
-  weighted_graph G(n);
-  weight_map weights = boost::get(boost::edge_weight, G);
-  
-  //std::vector<std::vector<int>> edges(e, std::vector<int>(s+2, 0));
-  
-  edge_desc E;
+  std::vector<std::vector<int>> edges(e, std::vector<int>(s+2, 0));
+
   for(int i=0; i<e; i++){
     int t_1, t_2; std::cin >> t_1 >> t_2;
-    int minimum = INT_MAX;
+    edges[i][0] = t_1; edges[i][1] = t_2;
     for(int j=0; j<s; j++){
       int w; std::cin >> w;
-      minimum = std::min(minimum, w);
+      edges[i][2+j] = w;
     }
-    E = boost::add_edge(t_1, t_2, G).first; weights[E] = minimum;
   }
-  
-  for(int i=0; i<s; i++){
-    int h; std::cin >> h;
-  }
-
-  std::vector<edge_desc> total_hive = kruskal(G);
   
   weighted_graph g(n);
   weight_map weights_ = boost::get(boost::edge_weight, g);
-  
   edge_desc e_;
-  for (std::vector<edge_desc>::iterator it = total_hive.begin(); it != total_hive.end(); ++it) {
-    int x = boost::source(*it, G);
-    int y = boost::target(*it, G);
-    int w = weights[*it];
-    e_ = boost::add_edge(x, y, g).first; weights_[e_]=w;
-  }
   
+  //For each species I build its mst and I store all of them. Then I merge them in
+  //a full graph on which I run Dijkstra
+  for(int i=0; i<s; i++){
+    
+    int h; std::cin >> h;
+    weighted_graph G(n);
+    weight_map weights = boost::get(boost::edge_weight, G);
+    edge_desc E;
+    
+    for(int j=0; j<e; j++){
+      E = boost::add_edge(edges[j][0], edges[j][1], G).first; weights[E]=edges[j][2+i];
+    }
+    
+    std::vector<edge_desc> mst; 
+    boost::kruskal_minimum_spanning_tree(G, std::back_inserter(mst));
+    
+    for(std::vector<edge_desc>::iterator it = mst.begin(); it != mst.end(); ++it) {
+      e_ = boost::add_edge(boost::source(*it, g), boost::target(*it, g), g).first; 
+      weights_[e_] = weights[*it];
+    }
+    
+  }
   
   std::cout << dijkstra_dist(g, a, b) << "\n";
 
